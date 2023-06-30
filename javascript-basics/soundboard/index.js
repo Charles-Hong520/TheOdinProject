fe771925ce27b20ee442f4198d71472d741d19ec
Charles-Hong520 <chong039@ucr.edu>
2023-06-30 15:23:13 -0700

    added clicking

--- javascript-basics/soundboard/index.js
+++ javascript-basics/soundboard/index.js
@@ -1,19 +1,38 @@
-function stopTransition(ev) {
-  const key = document.querySelector(`.key[code = "${ev.code}"]`);
-  key.classList.remove('playing');
-}
+
 
 
 function playSound(ev) {
   const audio = document.querySelector(`audio[code="${ev.code}"]`);
     
   if(!audio) return;
-  const key = document.querySelector(`.key[code="${ev.code}"]`);
-  audio.play();
+  const key = document.querySelector(`.key[id="${ev.code}"]`);
   audio.currentTime = 0;
+  audio.play();
   key.classList.add('playing');
+  setTimeout(() => {
+    key.classList.remove('playing');
+  }, audio.duration*1000);
+}
+
+
+function test(ev) {
+  console.log(ev.target);
+  const ct = ev.currentTarget;
+  const id = ct.getAttribute('id');
+  const audio = document.querySelector(`audio[code="${id}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  ct.classList.add('playing');
+  setTimeout(() => {
+    ct.classList.remove('playing');
+  }, audio.duration*1000);
 }
 
 
 window.addEventListener('keydown', playSound);
-window.addEventListener('keyup', stopTransition);
+
+var divs = document.querySelectorAll(".key");
+for(var i = 0; i < divs.length; i++){
+  divs[i].addEventListener('click',test);
+}
+
