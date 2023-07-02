b767126ccef4fec241129a2019ac22d661ae8e6d
Charles-Hong520 <chong039@ucr.edu>
2023-07-02 16:17:24 -0700

    finished rps-ui

--- javascript-basics/rps/rps.js
+++ javascript-basics/rps/rps.js
@@ -1,27 +1,85 @@
-nameToInt = {}
-nameToInt['rock'] = 1;
-nameToInt['paper'] = 2;
-nameToInt['scissor'] = 3;
-console.log("Hello, this is rock paper scissor game vs computer");
-
-function round() {
-  userChoice = prompt("Type 'Rock', 'Paper', or 'Scissor' to select your choice").toLowerCase();
-  if(!(userChoice in nameToInt)) {
-    console.log("Please enter a valid choice. Try Again.");
-    return;
+let nameToInt = {};
+let intToName = {};
+let userScore = 0;
+let computerScore = 0;
+let roundNum = 0;
+function gameover() {
+  if(userScore>=5 || computerScore>=5) {
+    document.querySelector(".rock").removeEventListener("click", roundrock);
+    document.querySelector(".paper").removeEventListener("click",roundpaper);
+    document.querySelector(".scissors").removeEventListener("click", roundscissors);
+    const winner = document.querySelector(".score");
+    let message = "";
+    if(userScore>=5) {
+      message = "You win!";
+    } else {
+      message = "You lose!";
+    }
+    const winnermessage = document.createElement("div");
+    winnermessage.innerText = message;
+    winner.appendChild(winnermessage);
+    console.log("gameover");
   }
+}
+
+function round(userChoice) {
+  roundNum++;
+  const roundlog = document.querySelector(".roundlog");
+  const round = document.createElement("p");
+    
+
 
   userChoice = nameToInt[userChoice];
-  computerChoice = Math.floor(Math.random() * 3);
-  
+  let computerChoice = 1 + Math.floor(Math.random() * 3);
+  let roundlogtext = `Round ${roundNum}: `;
+  roundlogtext += `You picked ${intToName[userChoice]}. CPU picked ${intToName[computerChoice]}. `;
+
   if(computerChoice==userChoice) {
-    console.log("It's a tie!");
+    roundlogtext += "It's a tie!";
   } else if(computerChoice%3 + 1 == userChoice) {
-    console.log("You win!");
+    roundlogtext += "You win the round.";
+    userScore++;
+    const span_userscore = document.querySelector(".userscore");
+    span_userscore.innerText = `${userScore}`;
   } else {
-    console.log("You lose!");
+    roundlogtext += "You lose the round.";
+    computerScore++;
+    const span_compscore = document.querySelector(".compscore");
+    span_compscore.innerText = `${computerScore}`;
   }
+
+  round.innerText = roundlogtext;
+  roundlog.appendChild(round);
+
+  gameover();
 }
-for(let i = 0; i < 5; i++) {
-  round();
+
+function roundrock() {
+  round('rock');
 }
+
+function roundpaper() {
+  round('paper');
+}
+
+function roundscissors() {
+  round('scissors');
+}
+function setup() {
+
+  nameToInt['rock'] = 1;
+  nameToInt['paper'] = 2;
+  nameToInt['scissors'] = 3;
+  for(let k in nameToInt) {
+    intToName[nameToInt[k]] = k;
+  }
+
+  roundNum = 0;
+  computerScore = 0;
+  userScore = 0;
+  document.querySelector(".rock").addEventListener("click", roundrock);
+  document.querySelector(".paper").addEventListener("click",roundpaper);
+  document.querySelector(".scissors").addEventListener("click", roundscissors);
+}
+
+setup();
